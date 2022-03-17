b9d8c9d510b230956c52050d62105963a4b9700c
Radha <rkunnattur@gmail.com>
2022-03-17 21:54:51 +0530

    Practice code

--- src/main/java/interviewbit/dynamicPrograming/DiceThrow.java
+++ src/main/java/interviewbit/dynamicPrograming/DiceThrow.java
@@ -1,5 +1,35 @@
 package src.main.java.interviewbit.dynamicPrograming;
 
+/*
+Dice throw
+
+Problem Description
+Given sum A, number of ways you can get that sum with dice roll[1-6].
+As the number of ways can be large return its modulo by 1e9 + 7.
+
+Problem Constraints
+1 <= A <= 10 2
+
+Input Format
+The first argument is the integer A.
+
+Output Format
+
+Return an integer .
+
+Example Input
+Input 1: A = 3
+Input 2: A = 4
+
+Example Output
+Output 1: 4
+Output 2: 8
+
+Example Explanation
+Explanation 1: The four possible ways to obtain 3 are: [1, 1, 1], [1, 2], [2, 1] and [3].
+Explanation 2: The eight possible ways to obrain 8 are: [1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [1, 3], [3, 1], [2, 2], [4].
+ */
+
 public class DiceThrow {
     int M = (int) (1e9) + 7;
     public int solve(int A) {
