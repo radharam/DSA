dea3e7ac4a0dbd5fa0cc9d3c466a66a1b6389d4c
Radha Kunnattur <radzram@gmail.com>
2022-06-16 19:11:46 +0530

    Create NumberConnectedComponentsUndirectedGraph.java

---
+++ src/main/java/leetcode/graph/dsu/NumberConnectedComponentsUndirectedGraph.java
@@ -0,0 +1,59 @@
+
+/*
+  https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+  323. Number of Connected Components in an Undirected Graph
+
+*/
+
+public Class NumberConnectedComponentsUndirectedGraph {
+    
+    // path compression, rank hueristic
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+        public UnionFind(int A){
+            this.parent = new int[A+1];
+            this.rank = new int[A+1];
+            for(int i=0; i<A+1; i++){
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public void union(int x, int y){
+            int px = find(x);
+            int py = find(y);
+
+            if(px == py) return;
+
+            if(this.rank[px] >= this.rank[py]){
+                this.parent[py] = px;
+                this.rank[px] += this.rank[py];
+            }
+            else{
+                this.parent[px] = py;
+                this.rank[py] += this.rank[px];
+            }
+        }
+
+        public int find(int x){
+            if(this.parent[x] != x) this.parent[x] = find(this.parent[x]);
+            return this.parent[x];
+        }
+
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n+1);
+        for(int[] edge: edges) {
+            uf.union(edge[0], edge[1]);
+        }
+        
+        Set<Integer> cntOfDistinctConnComp = new HashSet();
+        for(int i = 0; i < n; i++) {
+            cntOfDistinctConnComp.add(uf.find(i));
+        }
+        
+        return cntOfDistinctConnComp.size();
+    }
+}
