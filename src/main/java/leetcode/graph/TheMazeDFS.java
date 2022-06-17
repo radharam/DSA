7126faec0585252319c603a3a90e6b5964946f92
Radha Kunnattur <radzram@gmail.com>
2022-06-17 17:15:30 +0530

    Create TheMazeDFS.java

---
+++ src/main/java/leetcode/graph/TheMazeDFS.java
@@ -0,0 +1,47 @@
+/* 
+https://leetcode.com/problems/the-maze/
+490. The Maze
+
+check if there is a path from source to destination with DFS approach
+
+*/
+
+public class TheMazeDFS {
+    // DFS Aproach
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        return dfs(maze, start, destination, visited);
+    }
+    
+    public boolean dfs(int[][] maze, int[] start, int[] destination, boolean[][] visited) {
+        // Base condition
+        
+        // if visited, don't explore further
+        if(visited[start[0]][start[1]]) return false;
+        // check if reached destination
+        if(start[0] == destination[0] && start[1] == destination[1]) return true;
+        
+        visited[start[0]][start[1]] = true;
+        
+        int u = start[0] - 1, r = start[1] + 1, d = start[0] + 1, l = start[1] - 1;
+        
+        // move up
+        while(u >= 0 && maze[u][start[1]] == 0) u--;
+        if(dfs(maze, new int[] {u+1, start[1]}, destination, visited)) return true;
+        
+        // move right
+        while(r < maze[0].length && maze[start[0]][r] == 0) r++;
+        if(dfs(maze, new int[] {start[0], r-1}, destination, visited)) return true;
+        
+        // move down
+        while(d < maze.length && maze[d][start[1]] == 0) d++;
+        if(dfs(maze, new int[] {d-1, start[1]}, destination, visited)) return true;
+        
+        // move left
+        while(l >= 0 && maze[start[0]][l] == 0) l--;
+        if(dfs(maze, new int[] {start[0],l+1}, destination, visited)) return true;
+        
+        return false;
+        
+    }
+}
