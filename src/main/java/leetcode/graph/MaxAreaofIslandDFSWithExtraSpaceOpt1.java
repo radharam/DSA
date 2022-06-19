a72a6d3f6485cad11de2ae9825856a74789f3230
Radha Kunnattur <radzram@gmail.com>
2022-06-19 19:26:07 +0530

    Update MaxAreaofIslandDFSWithExtraSpaceOpt1.java

--- src/main/java/leetcode/graph/MaxAreaofIslandDFSWithExtraSpaceOpt1.java
+++ src/main/java/leetcode/graph/MaxAreaofIslandDFSWithExtraSpaceOpt1.java
@@ -8,10 +8,12 @@
 class MaxAreaofIslandDFSWithExtraSpaceOpt1 {
     public int maxAreaOfIsland(int[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length]; // define once nn reuse
+        int[][] dirs = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         int maxArea = 0;
         for(int r = 0; r < grid.length; r++) {
             for(int c = 0; c < grid[0].length; c++) {
-                maxArea = Math.max(maxArea, computeAreaDFSWithExtraSpace(grid, r, c, visited));
+                //maxArea = Math.max(maxArea, computeAreaDFSWithExtraSpace(grid, r, c, visited));
+                maxArea = Math.max(maxArea, computeAreaDFSWithExtraSpace(grid, r, c, visited, dirs));
             }
         }
         
@@ -33,7 +35,20 @@ class MaxAreaofIslandDFSWithExtraSpaceOpt1 {
         
         if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0 || visited[r][c]) return 0;
         
-        int[][] dirs = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        visited[r][c] = true;
+        int area = 1;
+        for(int[] dir: dirs) {
+            int x = r + dir[0], y = c + dir[1];
+            area += computeAreaDFSWithExtraSpace(grid, x, y, visited, dirs);
+        }
+        
+        return area;
+    }
+    
+    // changed dirs as pass in param
+    public int computeAreaDFSWithExtraSpaceApproach3(int[][] grid, int r, int c, boolean[][] visited, int[][] dirs) {
+        
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0 || visited[r][c]) return 0;
         
         visited[r][c] = true;
         int area = 1;
