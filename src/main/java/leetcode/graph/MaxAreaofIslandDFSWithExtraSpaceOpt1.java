92ab7901bd36a7df0e2ff252b87dc21ff665bc10
Radha Kunnattur <radzram@gmail.com>
2022-06-19 19:00:55 +0530

    Create MaxAreaofIslandDFSWithExtraSpaceOpt1.java

---
+++ src/main/java/leetcode/graph/MaxAreaofIslandDFSWithExtraSpaceOpt1.java
@@ -0,0 +1,24 @@
+class MaxAreaofIslandDFSWithExtraSpaceOpt1 {
+    public int maxAreaOfIsland(int[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length]; // define once nn reuse
+        int maxArea = 0;
+        for(int r = 0; r < grid.length; r++) {
+            for(int c = 0; c < grid[0].length; c++) {
+                maxArea = Math.max(maxArea, computeAreaDFSWithExtraSpace(grid, r, c, visited));
+            }
+        }
+        
+        return maxArea;
+    }
+    
+    public int computeAreaDFSWithExtraSpace(int[][] grid, int r, int c, boolean[][] visited) {
+        
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0 || visited[r][c]) return 0;
+        
+        visited[r][c] = true;
+        return (1 + computeAreaDFSWithExtraSpace(grid, r-1, c, visited) +
+                    computeAreaDFSWithExtraSpace(grid, r+1, c, visited) +
+                    computeAreaDFSWithExtraSpace(grid, r, c-1, visited) +
+                    computeAreaDFSWithExtraSpace(grid, r, c+1, visited));
+    }
+}
